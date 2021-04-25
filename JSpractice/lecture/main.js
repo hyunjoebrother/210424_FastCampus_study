d9469e577168f198b95e98111e20b36e741066a7
hyunjoebrother <hyunjoe31@gmail.com>
2021-04-25 12:51:17 +0900

    210425_FC_JS_study

---
+++ JSpractice/lecture/main.js
@@ -0,0 +1,12 @@
+console.log('connect lol');
+
+let fruits = ['Apple', 'Banana', 'Cherry'];
+// fruits라는 변수
+
+console.log(fruits[0]); // 0번째 item에 접근해서 console.log로 출력
+console.log(fruits[1]);
+console.log(fruits[2]);
+
+console.log(new Date('2021-01-30').getDay()); // 6, 토요일
+console.log(new Date('2021-01-31').getDay()); // 0, 일요일
+console.log(new Date('2021-02-01').getDay()); // 1, 월요일
