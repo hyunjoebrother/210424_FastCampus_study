98c80ef7e3ca757b2422e2daa41b470fd0506bbd
hyunjoebrother <hyunjoe31@gmail.com>
2021-04-26 15:05:30 +0900

    210426_FC_JS_study

--- JSpractice/lecture/main.js
+++ JSpractice/lecture/main.js
@@ -1,12 +1,39 @@
-console.log('connect lol');
+// console.log('connect lol');
 
-let fruits = ['Apple', 'Banana', 'Cherry'];
-// fruits라는 변수
+// let fruits = ['Apple', 'Banana', 'Cherry'];
+// // fruits라는 변수
 
-console.log(fruits[0]); // 0번째 item에 접근해서 console.log로 출력
-console.log(fruits[1]);
-console.log(fruits[2]);
+// console.log(fruits[0]); // 0번째 item에 접근해서 console.log로 출력
+// console.log(fruits[1]);
+// console.log(fruits[2]);
 
-console.log(new Date('2021-01-30').getDay()); // 6, 토요일
-console.log(new Date('2021-01-31').getDay()); // 0, 일요일
-console.log(new Date('2021-02-01').getDay()); // 1, 월요일
+// // getDay()라는 Method
+// console.log(new Date('2021-01-30').getDay()); // 6, 토요일
+// console.log(new Date('2021-01-31').getDay()); // 0, 일요일
+// console.log(new Date('2021-02-01').getDay()); // 1, 월요일
+
+
+// 자료형 1. String (문자 data) -> 띠옴표 사용
+let myName = "Mei";
+let email = 'clara_31@naver.com';
+let hello = `Hello ${myName}?!`
+
+console.log(myName);
+console.log(email);
+console.log(hello);
+
+
+// 자료형 2. Number (숫자 data) -> 정수 및 부동소수점 숫자
+let number = 123;
+let opacity = 1.57;
+
+console.log(number);
+console.log(opacity);
+
+
+// 자료형 3. Boolean (논리 data) -> True & False
+let checked = true;
+let isShow = false;
+
+console.log(checked);
+console.log(isShow);
