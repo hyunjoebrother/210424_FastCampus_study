7e37ddf7c1e91095d39ab65d08521afb1893b89d
hyunjoebrother <hyunjoe31@gmail.com>
2021-04-29 23:18:26 +0900

    210429_FC_JS_study

--- JSpractice/lecture/main.js
+++ JSpractice/lecture/main.js
@@ -13,62 +13,69 @@
 // console.log(new Date('2021-02-01').getDay()); // 1, 월요일
 
 
-// 자료형 1. String (문자 data) -> 띠옴표 사용
-let myName = "Mei";
-let email = 'clara_31@naver.com';
-let hello = `Hello ${myName}?!`
+// // 자료형 1. String (문자 data) -> 띠옴표 사용
+// let myName = "Mei";
+// let email = 'clara_31@naver.com';
+// let hello = `Hello ${myName}?!`
 
-console.log(myName);
-console.log(email);
-console.log(hello);
-
-
-// 자료형 2. Number (숫자 data) -> 정수 및 부동소수점 숫자
-let number = 123;
-let opacity = 1.57;
-
-console.log(number);
-console.log(opacity);
+// console.log(myName);
+// console.log(email);
+// console.log(hello);
 
 
-// 자료형 3. Boolean (논리 data) -> True & False
-let checked = true;
-let isShow = false;
+// // 자료형 2. Number (숫자 data) -> 정수 및 부동소수점 숫자
+// let number = 123;
+// let opacity = 1.57;
 
-console.log(checked);
-console.log(isShow);
-
-
-// 자료형 4. Undefined -> 값이 할당되지 않은 상태를 나타냄
-let undef; // 값이 없음
-let obj = {abc : 123}; // abc라는 속성에 숫자 값 넣어주는 객체 {}
-
-console.log(undef); // undefined
-console.log(obj.abc); // abc라는 속성의 값은 무엇인가 123
-console.log(obj.xyz); // undefined
+// console.log(number);
+// console.log(opacity);
 
 
-// 자료형 5. Null -> 어떤 값이 의도적으로 비어있음을 의미
-let empty = null;
-console.log(empty);
+// // 자료형 3. Boolean (논리 data) -> True & False
+// let checked = true;
+// let isShow = false;
+
+// console.log(checked);
+// console.log(isShow);
 
 
-// 자료형 6. Object : 객체 data -> 여러 data를 { : } 형태로 저장
-let user = {
-    name : 'Mei', // 문자 data
-    age : 17, // 숫자 data
-    inValid : true // bool data
-};
+// // 자료형 4. Undefined -> 값이 할당되지 않은 상태를 나타냄
+// let undef; // 값이 없음
+// let obj = {abc : 123}; // abc라는 속성에 숫자 값 넣어주는 객체 {}
 
-console.log(user.name);
-console.log(user.age);
-console.log(user.inValid);
-console.log(user);
+// console.log(undef); // undefined
+// console.log(obj.abc); // abc라는 속성의 값은 무엇인가 123
+// console.log(obj.xyz); // undefined
 
 
-// 자료형 7. Array : 배열 data -> 여러 data를 순차적으로 저장
-let fruits = ['Apple', 'Banana', 'Cherry'];
+// // 자료형 5. Null -> 어떤 값이 의도적으로 비어있음을 의미
+// let empty = null;
+// console.log(empty);
 
-console.log(fruits[0]);
-console.log(fruits[1]);
-console.log(fruits[2]);
+
+// // 자료형 6. Object : 객체 data -> 여러 data를 { : } 형태로 저장
+// let user = {
+//     name : 'Mei', // 문자 data
+//     age : 17, // 숫자 data
+//     inValid : true // bool data
+// };
+
+// console.log(user.name);
+// console.log(user.age);
+// console.log(user.inValid);
+// console.log(user);
+
+
+// // 자료형 7. Array : 배열 data -> 여러 data를 순차적으로 저장
+// let fruits = ['Apple', 'Banana', 'Cherry'];
+
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+
+// 변수, 예약어
+
+let a = 2;
+let b = 5;
+
+console.log(a+b);
