39d80e8e58a909e7fe72549c98b4e793756fbc07
hyunjoebrother <hyunjoe31@gmail.com>
2021-05-01 22:43:05 +0900

    210501_FC_JS_study

--- JSpractice/lecture/main.js
+++ JSpractice/lecture/main.js
@@ -178,5 +178,24 @@
 
 
 // DOM API
-let boxEl = document.querySelector('.box'); // .box : css 클래스 선택자
-console.log(boxEl);
+// let boxEl = document.querySelector('.box'); // .box : css 클래스 선택자
+// console.log(boxEl);
+
+// HTML 요소 Element 1개 검색/찾기
+const boxEl = document.querySelector('.box');
+
+// HTML 요소에 적용할 수 있는 메소드
+boxEl.addEventListener();
+
+// 인수 Argument를 추가 가능
+boxEl.addEventListener(1,2);
+
+// 1 - 이벤트 Event - 상황
+boxEl.addEventListener('click', 2);
+
+// 2 - 핸들러 Handler - 실행할 함수
+boxEl.addEventListener('click', function() 
+{
+    console.log('Click~!');
+} );
+
