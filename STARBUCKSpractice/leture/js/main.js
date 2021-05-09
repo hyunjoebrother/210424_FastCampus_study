c4caf8704583f513a64b5710685ae85375207764
hyunjoebrother <hyunjoebrother>
2021-05-09 12:16:32 +0900

    210509_FC_STARBUCKS_practice

--- STARBUCKSpractice/leture/js/main.js
+++ STARBUCKSpractice/leture/js/main.js
@@ -15,3 +15,19 @@ searchInputEl.addEventListener('blur', function() { // focus 해제
     searchEl.classList.remove('focused');
     searchInputEl.setAttribute('placeholder', '');
 });
+
+
+// 스크롤 내리면 배너 사라짐
+const badgeEl = document.querySelector('header .badges');
+// 브라우저 창 window, 우리가 보고 있는 화면 자체
+// window.addEventListener('scroll', function() {
+//     console.log('scroll!'); // scroll할 때마다 출력
+// });    
+window.addEventListener('scroll', _.throttle(function() {
+    console.log('scroll!');
+    if (window.scrollY) {
+            
+    }
+}, 300 ));// 0.3s마다 함수 다수 실행 부하 (실행 함수 제한 -> 숫자 천천히 출력) 
+
+// _.throttle(함수, ms단위로 시간 추가)
