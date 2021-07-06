9df037094f2bf3690099ae77010b434ce4b87179
hyunjoebrother <hyunjoe31@gmail.com>
2021-07-06 18:29:52 +0900

    210706_FC_JS_essentials

--- JSessentials/lecture/JStest/main.js
+++ JSessentials/lecture/JStest/main.js
@@ -1,2 +1,19 @@
-console.log("hello world");
-console.log(123);
+import getType from "./getType";
+
+console.log(typeof "hello world");
+console.log(typeof 123);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof {});
+console.log(typeof []);
+
+// function getType(data) {
+//   //   return Object.prototype.toString.call(data);
+//   return Object.prototype.toString.call(data).slice(8, -1);
+// }
+
+console.log(getType(123));
+console.log(getType(null));
+console.log(getType({}));
+console.log(getType([]));
