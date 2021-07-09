6208325485945f670880c771808170501b39a2c4
hyunjoebrother <hyunjoe31@gmail.com>
2021-07-09 23:09:51 +0900

    210709_FC_JS_essentials

--- JSessentials/lecture/JStest/main2.js
+++ JSessentials/lecture/JStest/main2.js
@@ -17,3 +17,79 @@ function sum() {
   console.log(arguments);
   return arguments[0] + arguments[1];
 }
+
+// 화살표 함수
+const double = function (x) {
+  return x * 2;
+};
+
+console.log("double: ", double(7));
+
+const doubleArrow = (x) => {
+  return x * 2;
+};
+
+const doubleArrow = (x) => x * 2;
+const doubleArrow = (x) => ({
+  name: "Mei",
+});
+
+console.log("doubleArrow: ", doubleArrow(7));
+
+// IIFE (즉시 실행 함수)
+const ii = 7;
+function double() {
+  console.log(ii * 2);
+}
+double();
+
+(function () {
+  console.log(ii * 2);
+})();
+
+// 호이스팅 (Hoisting)
+const h = 7;
+
+double();
+
+function double() {
+  console.log(h * 2);
+}
+
+// 타이머 함수
+
+setTimeout(function () {
+  console.log("Yo!");
+}, 3000);
+
+const timer = setTimeout(() => {
+  console.log("Yo!");
+}, 3000);
+
+const h1El = document.querySelector("h1");
+h1El.addEventListener("click", () => {
+  clearTimeout(timer);
+});
+
+const timer2 = setInterval(() => {
+  console.log("yoyoyo");
+});
+
+const h1El = document.querySelector("h1");
+h1El.addEventListener("click", () => {
+  clearInterval(timer2);
+});
+
+// 콜백
+function timeout(cb) {
+  setTimeout(() => {
+    console.log("meimei");
+    cb();
+  }, 3000);
+}
+
+timeout(() => {
+  console.log("done"); // 먼저 실행되고 그 후에 3초 후에 진행
+});
+
+console.log("end"); // 먼저 실행되고 그 후에 3초 후에 진행
